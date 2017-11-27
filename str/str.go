4e22cc336be33810178442d76581060e7b6505de
gaoqilin <gaoqilin@360.cn>
2017-11-27 15:35:36 +0800

    add str

---
+++ str/str.go
@@ -0,0 +1,28 @@
+package str
+
+func Chr(c rune) string {
+	return string(c)
+}
+
+func Ord(c string) []rune {
+	return []rune(c)
+}
+
+func Substr(str string, begin, length int) string {
+	lth := len(str)
+
+	// 简单的越界判断
+	if begin < 0 {
+		begin = 0
+	}
+	if begin >= lth {
+		begin = lth
+	}
+	end := begin + length
+	if end > lth {
+		end = lth
+	}
+
+	// 返回子串
+	return string(str[begin:end])
+}
