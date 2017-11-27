3cd7f723b0e10c61b9e2d940dcabc66c1f6bc481
gaoqilin <gaoqilin@360.cn>
2017-11-27 16:08:53 +0800

    add strToInt32

--- str/str.go
+++ str/str.go
@@ -1,5 +1,11 @@
 package str
 
+import (
+	"fmt"
+	"math"
+	"strconv"
+)
+
 func Chr(c rune) string {
 	return string(c)
 }
@@ -9,7 +15,7 @@ func Ord(c string) []rune {
 }
 
 func Substr(str string, begin, length int) string {
-	lth := len(str)	
+	lth := len(str)
 	if begin < 0 {
 		begin = 0
 	}
@@ -20,6 +26,17 @@ func Substr(str string, begin, length int) string {
 	if end > lth {
 		end = lth
 	}
-	
+
 	return string(str[begin:end])
+}
+
+func StrToInt32(str string) (int32, error) {
+	num, err := strconv.ParseInt(str, 10, 0)
+	if err != nil {
+		return 0, fmt.Errorf("\"%s\" is not integer", str)
+	}
+	if num > math.MaxInt32 || num < math.MinInt32 {
+		return 0, fmt.Errorf("%d is not 32-bit integer", num)
+	}
+	return int32(num), nil
 }
