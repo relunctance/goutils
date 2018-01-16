282be3a12165a2450008fa1be652779b37e7f84c
gaoqilin <gaoqilin@360.cn>
2018-01-16 22:38:06 +0800

    add CheckSliceEqual

--- str/str.go
+++ str/str.go
@@ -57,3 +57,15 @@ func ParseStr(str string) url.Values {
 	}
 	return u.Query()
 }
+
+func CheckStringSliceEqual(x, y []string) bool {
+	if len(x) != len(y) {
+		return false
+	}
+	for i := range x {
+		if x[i] != y[i] {
+			return false
+		}
+	}
+	return true
+}
