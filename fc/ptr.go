75dfb7f2bceebdda052167be543dbca8c7b5dafb
gaoqilin <gaoqilin@360.cn>
2018-03-29 11:43:40 +0800

    增加检测方法, 和检测字段是否存在判断

--- fc/ptr.go
+++ fc/ptr.go
@@ -1,9 +1,38 @@
 package fc
 
-import "reflect"
+import (
+	"fmt"
+	"reflect"
+)
 
 //判断是否是指针
 func IsPtr(value interface{}) bool {
 	v := reflect.ValueOf(value)
 	return v.Kind() == reflect.Ptr
+}
+
+// 检测字段是否存在
+// 支持[private]字段检测
+// 支持[public]字段检测
+func FieldExists(fieldName string, ptr interface{}) bool {
+	if !IsPtr(ptr) {
+		panic(fmt.Errorf("must input a *ptr"))
+	}
+	v := reflect.ValueOf(ptr)
+	e := v.Elem()
+	t := e.Type()
+	_, ok := t.FieldByName(fieldName)
+	return ok
+}
+
+// 检测方法是否存在
+// 不支持[private]方法检测
+// 只支持[public]方法检测,私有方法无法检测出来
+func MethodExists(methodName string, ptr interface{}) bool {
+	if !IsPtr(ptr) {
+		panic(fmt.Errorf("must input a *ptr"))
+	}
+	typ := reflect.TypeOf(ptr)
+	_, ok := typ.MethodByName(methodName)
+	return ok
 }
--- fc/ptr_test.go
+++ fc/ptr_test.go
@@ -8,6 +8,14 @@ import (
 
 type User struct {
 	Name string
+	pass string
+}
+
+func (u *User) GetName() string {
+	return u.Name
+}
+func (u *User) getpass() string {
+	return u.pass
 }
 
 func TestIsPtr(t *testing.T) {
@@ -19,5 +27,33 @@ func TestIsPtr(t *testing.T) {
 		convey.So(IsPtr(map[string]string{"username": "hello"}), convey.ShouldBeFalse)
 		convey.So(IsPtr(User{Name: "hello"}), convey.ShouldBeFalse)
 		convey.So(IsPtr(&User{Name: "hello"}), convey.ShouldBeTrue)
+	})
+}
+
+//
+func TestMethodExists(t *testing.T) {
+	convey.Convey("测试检测方法是否存在MethodExists():", t, func() {
+		user1 := &User{Name: "hello", pass: "abc"}
+		convey.So(MethodExists("GetName", user1), convey.ShouldBeTrue)
+		convey.So(MethodExists("getpass", user1), convey.ShouldBeFalse)
+		convey.So(MethodExists("Getname", user1), convey.ShouldBeFalse)
+		convey.So(MethodExists("a", user1), convey.ShouldBeFalse)
+		user2 := User{Name: "hello", pass: "abc"}
+		convey.So(func() {
+			MethodExists("a", user2)
+		}, convey.ShouldPanic)
+	})
+}
+
+func TestFieldExists(t *testing.T) {
+	convey.Convey("测试检测方法是否存在FieldExists():", t, func() {
+		user1 := &User{Name: "hello", pass: "abc"}
+		convey.So(FieldExists("Name", user1), convey.ShouldBeTrue)
+		convey.So(FieldExists("pass", user1), convey.ShouldBeTrue)
+		convey.So(FieldExists("a", user1), convey.ShouldBeFalse)
+		user2 := User{Name: "hello", pass: "abc"}
+		convey.So(func() {
+			FieldExists("a", user2)
+		}, convey.ShouldPanic)
 	})
 }
