3b2dc38df9e78c5299c587b70467a5b37e82956b
gaoqilin <gaoqilin@360.cn>
2018-06-05 17:51:48 +0800

    增加round

--- math/math.go
+++ math/math.go
@@ -1,5 +1,10 @@
 package math
 
+import (
+	"fmt"
+	"strconv"
+)
+
 //返回较小的值
 func MinInt(x, y int) int {
 	if x < y {
@@ -14,4 +19,14 @@ func MinInt64(x, y int64) int64 {
 		return x
 	}
 	return y
+}
+
+func Round(value float64, precision int) (f float64) {
+	if precision < 0 {
+		panic(fmt.Errorf("round precision must gt 0"))
+	}
+	format := fmt.Sprintf("%df", precision)
+	s := fmt.Sprintf("%0."+format, value)
+	f, _ = strconv.ParseFloat(s, 64)
+	return
 }
