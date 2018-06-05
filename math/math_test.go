734ba0bc3d939fb916f64a9f621a6c8b2c95e653
gaoqilin <gaoqilin@360.cn>
2018-06-05 17:52:58 +0800

    增加单元测试

--- math/math_test.go
+++ math/math_test.go
@@ -1,6 +1,7 @@
 package math
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/smartystreets/goconvey/convey"
@@ -24,5 +25,25 @@ func TestMinInt(t *testing.T) {
 			num = MinInt64(a, b)
 			convey.So(num, convey.ShouldEqual, b)
 		})
+	})
+}
+func TestRound(t *testing.T) {
+	convey.Convey("测试math:", t, func() {
+		//   example 1: round(1241757, -3)
+		//   returns 1: 1242000
+		//   example 2: round(3.6)
+		//   returns 2: 4
+		//   example 3: round(2.835, 2)
+		//   returns 3: 2.84
+		//   example 4: round(1.1749999999999, 2)
+		//   returns 4: 1.17
+		//   example 5: round(58551.799999999996, 2)
+		//   returns 5: 58551.8
+		fmt.Println(Round(1, 0))
+		fmt.Println(Round(124, 3))
+		fmt.Println(Round(3.6, 0))
+		fmt.Println(Round(2.835, 2))
+		fmt.Println(Round(1.1749999999999, 2))
+		fmt.Println(Round(58551.799999999996, 3))
 	})
 }
