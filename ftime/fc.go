26fba3faa448603d205bfe942a57345a41706ae7
gaoqilin <gaoqilin@360.cn>
2018-06-05 16:38:44 +0800

    time 增加microtime

---
+++ ftime/fc.go
@@ -0,0 +1,8 @@
+package ftime
+
+// 返回微秒 , unix
+func Microtime() (millisecond float64, unix int) {
+	t := time.Now()
+	millisecond, unix = float64(t.Nanosecond())/1000/1000/1000, t.Unix()
+	return
+}
