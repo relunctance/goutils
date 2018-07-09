bbd53cb7e67b77afd81ec6e7f08d47503fa53bcc
gaoqilin <gaoqilin@360.cn>
2018-07-09 12:33:51 +0800

    增加获取本机ip地址

---
+++ fc/ip.go
@@ -0,0 +1,21 @@
+package fc
+
+import (
+	"fmt"
+	"net"
+)
+
+func GetLocalHostIPv4() (string, error) {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return "", err
+	}
+	for _, addr := range addrs {
+		if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.String(), nil
+			}
+		}
+	}
+	return "", fmt.Errorf("can not get local host ip address")
+}
