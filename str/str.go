fe19ae6ffa720bcfe4fe82d56c3b9e4a5f784cee
gaoqilin <gaoqilin@360.cn>
2018-07-16 19:31:46 +0800

    add func

--- str/str.go
+++ str/str.go
@@ -98,3 +98,33 @@ func ByteFormat(i uint64) string {
 	}
 	return fmt.Sprintf("%f %s", j, a[pos])
 }
+
+//添加反斜杠
+func Addslashes(str string) string {
+	tmpRune := []rune{}
+	strRune := []rune(str)
+	for _, ch := range strRune {
+		switch ch {
+		case []rune{'\\'}[0], []rune{'"'}[0], []rune{'\''}[0]:
+			tmpRune = append(tmpRune, []rune{'\\'}[0])
+			tmpRune = append(tmpRune, ch)
+		default:
+			tmpRune = append(tmpRune, ch)
+		}
+	}
+	return string(tmpRune)
+}
+
+// stripslashes() 函数删除由 addslashes() 函数添加的反斜杠。
+func Stripslashes(str string) string {
+	dstRune := []rune{}
+	strRune := []rune(str)
+	strLenth := len(strRune)
+	for i := 0; i < strLenth; i++ {
+		if strRune[i] == []rune{'\\'}[0] {
+			i++
+		}
+		dstRune = append(dstRune, strRune[i])
+	}
+	return string(dstRune)
+}
