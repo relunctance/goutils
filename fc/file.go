d376c77edaf5a45dced86993307851fa7f2d7332
Gao.QiLin <334136724@qq.com>
2018-12-20 15:07:33 +0800

    readable
    add readable

--- fc/file.go
+++ fc/file.go
@@ -22,6 +22,15 @@ func IsWriteable(name string) bool {
     return false                                                                                                      
 }  
 
+// check file or dir is readable
+func IsReadable(name string) bool {                                                                                   
+    err := syscall.Access(name, syscall.O_RDONLY)                                                                     
+    if err == nil {
+        return true
+    }
+    return false
+}  
+
 // get file size by filepath
 func FileSize(filename string) int64 {
 	if info, err := os.Stat(filename); err == nil {
