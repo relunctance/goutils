66a1cc638f2b9cd4f27ba2f7c6d972cde579283a
Gao.QiLin <334136724@qq.com>
2020-02-05 13:56:07 +0800

    Update time_test.go

--- fc/time_test.go
+++ fc/time_test.go
@@ -25,3 +25,18 @@ func TestFstrtotime(t *testing.T) {
 		})
 	})
 }
+
+
+func TestFstime2(t *testing.T) {
+    ms := map[string]int64{
+        "2020-01-29 18:49:46": 1580294986,
+        "2020-01-30 02:49:46": 1580323786,
+        "2019-01-11 21:49:42": 1547214582,
+    }
+    for k, v := range ms {
+        ti, _ := Fstrtotime(k)
+        if ti.Unix() != v { // 不允许自动增加8小时
+            t.Fatalf("不允许自动增加8小时时区,should be === %d", v)
+        }
+    }
+}
