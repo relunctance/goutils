9a1677b2f1c3f94e95f40f4c82272401de37b6fe
gaoqilin <334136724@qq.com>
2020-11-28 22:37:22 +0800

    add SaveFile()

--- fc/file.go
+++ fc/file.go
@@ -2,8 +2,10 @@ package fc
 
 import (
 	"bytes"
+	"io"
 	"io/ioutil"
 	"os"
+	"path"
 	"syscall"
 )
 
@@ -60,4 +62,60 @@ func FileStrings(filename string) ([]string, error) {
 		data = append(data, string(line))
 	}
 	return data, nil
+}
+
+// SaveFile data save to file
+func SaveFile(path string, content []byte, bak bool) error {
+	tmp := path + ".tmp"
+	err := AttachFileDirectory(tmp)
+	if err != nil {
+		return err
+	}
+	if bak && IsExist(path) {
+		// bak
+		_ = CopyFileContents(path, path+".bak")
+	}
+	f, err := os.OpenFile(tmp, os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 0644)
+	if err != nil {
+		return err
+	}
+	_, err = f.Write(content)
+	if err != nil {
+		return err
+	}
+	_ = f.Close()
+	return os.Rename(tmp, path)
+}
+
+func AttachFileDirectory(fpath string) error {
+	baseDir := path.Dir(fpath)
+	info, err := os.Stat(baseDir)
+	if err == nil && info.IsDir() {
+		return nil
+	}
+	return os.MkdirAll(baseDir, 0755)
+}
+func CopyFileContents(src, dst string) (err error) {
+	in, err := os.Open(src)
+	if err != nil {
+		return
+	}
+	defer func() {
+		_ = in.Close()
+	}()
+	out, err := os.Create(dst)
+	if err != nil {
+		return
+	}
+	defer func() {
+		cerr := out.Close()
+		if err == nil {
+			err = cerr
+		}
+	}()
+	if _, err = io.Copy(out, in); err != nil {
+		return
+	}
+	err = out.Sync()
+	return
 }
